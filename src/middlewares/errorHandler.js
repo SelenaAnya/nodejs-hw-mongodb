945153260943d20ae4821ee87abd9938207f38c4
Anna Kovalenko <91620969+SelenaAnya@users.noreply.github.com>
2025-08-07 09:52:30 +0300

    0708 0952

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,11 @@
+const errorHandler = (err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(status).json({
+        status,
+        message: status === 500 ? 'Something went wrong' : message,
+        data: err.message,
+    });
+};
+
+export default errorHandler;
--- src/server.js
+++ src/server.js
@@ -2,12 +2,13 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import contactsRouter from './routers/contacts.js';
+import errorHandler from './middlewares/errorHandler.js';
+import notFoundHandler from './middlewares/notFoundHandler.js';
 
 const PORT = Number(process.env.PORT) || 3000;
 
 export const setupServer = () => {
     const app = express();
-
 
     app.use(
         pino({
@@ -23,21 +24,11 @@ export const setupServer = () => {
     app.use(cors());
     app.use(express.json());
 
-
     app.use(contactsRouter);
 
-    app.use('*', (req, res, next) => {
-        res.status(404).json({
-            message: 'Not found',
-        });
-    });
+    app.use('*', notFoundHandler);
 
-    app.use((err, req, res, next) => {
-        res.status(500).json({
-            message: 'Something went wrong',
-            error: err.message,
-        });
-    });
+    app.use(errorHandler);
 
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
