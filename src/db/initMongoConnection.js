69143b15c80bbd9a3d5e0f6d7410223fa0374925
Anna Kovalenko <91620969+SelenaAnya@users.noreply.github.com>
2025-09-04 20:24:59 +0300

    0409 2024

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,20 +1,17 @@
 import mongoose from 'mongoose';
+import { env } from '../utils/env.js';
 
-const initMongoConnection = async () => {
+export const initMongoConnection = async () => {
     try {
-        const mongoUrl = process.env.MONGODB_URL;
-
-        if (!mongoUrl) {
-            throw new Error('MONGODB_URL is not defined in environment variables');
-        }
-
-        await mongoose.connect(mongoUrl);
-
+        const user = env('MONGODB_USER');
+        const password = env('MONGODB_PASSWORD');
+        const url = env('MONGODB_URL');
+        const db = env('MONGODB_DB');
+        await mongoose.connect(
+            `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority`,
+        );
         console.log('Mongo connection successfully established!');
     } catch (error) {
-        console.error('Error while setting up mongo connection:', error);
-        throw error;
+        console.log('Error while setting up mongo connection', error);
     }
 };
-
-export default initMongoConnection;
--- src/utils/env.js
+++ src/utils/env.js
@@ -1,3 +1,8 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
 export const env = (name, defaultValue) => {
     const value = process.env[name];
 
