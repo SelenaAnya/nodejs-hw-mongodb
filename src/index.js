0c9903672406868a7277f91ec9d41531ed1ee62a
Anna Kovalenko <91620969+SelenaAnya@users.noreply.github.com>
2025-09-12 16:49:28 +0300

    1209 1649

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import setupServer from './server.js';
+// import setupServer from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
 import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
@@ -11,7 +11,7 @@ const bootstrap = async () => {
     await initMongoConnection();
     await createDirIfNotExists(TEMP_UPLOAD_DIR);
     await createDirIfNotExists(UPLOAD_DIR);
-    setupServer();
+    // setupServer();
 };
 
 void bootstrap();
